51b1d5bf0d2dfa667583c51e86bde563a22ee0f2
Ehya-Ag <ehyaagmohamed23@gmail.com>
2024-09-30 17:16:15 +0000

    lien du backend avec cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://gestion-recettes-three.vercel.app',
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
